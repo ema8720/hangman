6ddd0004f03dbb3ec67947f5f3fd8049bddfded9
John Correa <ema8720@gmail.com>
2021-05-04 10:04:16 -0700

    Created a method for making a guess and another method to give us the word puzzle back.

--- hangman.js
+++ hangman.js
@@ -7,9 +7,11 @@
 
 
 const Hangman = function (word, remainingGuesses) {
+    // set up a word instance property as an array of lower cases.
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c']
+    // setting up an instance property to store guessed letters.
+    this.guessedLetters = ['c','a']
 }
 
 
@@ -27,8 +29,27 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
-const game1 = new Hangman('cat', 2);
-console.log(game1.getPuzzle());
+// Create a method for making a guess. Calling it makeGuess
+// 1. Should accept a character for guessing.
+// 2. Should add unique guesses to list of guesses.
+// 3. Should decrement the guesses left if a unique guess isn't a match.
 
-const game2 = new Hangman('New Jersey', 4);
-console.log(game2.getPuzzle());
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess) 
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+}
+    if (isUnique && isBadGuess) {
+        this.remainingGuesses = this.remainingGuesses - 1
+        // this.remainingGuesses--
+    }
+}
+
+const game1 = new Hangman('cat', 2);
+    game1.makeGuess('c')
+    game1.makeGuess('t')
+    game1.makeGuess('z')
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
