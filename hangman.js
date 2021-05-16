bb7c3c7943109da814d723628f9338ff225502c9
John Correa <ema8720@gmail.com>
2021-05-15 18:17:58 -0700

    made updates to the makeGuess method by creating another option.

--- hangman.js
+++ hangman.js
@@ -34,26 +34,43 @@ Hangman.prototype.getPuzzle = function () {
 // 2. Should add unique guesses to list of guesses.
 // 3. Should decrement the guesses left if a unique guess isn't a match.
 
+// This is the first option
 Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess) 
-    if (isUnique) {
+    const isBadGuess = !this.word.includes(guess)
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+        if (isBadGuess && isUnique) {
+            this.remainingGuesses--
+            //this.remainingGuesses = this.remainingGuesses - 1 
+        }
+} 
+
+// This is a second option
+/*
+Hangman.prototype.makeGuess = function(guess){
+    var guess = guess.toLowerCase()
+    if (!this.guessedLetters.includes(guess) && !this.word.includes(guess)){
         this.guessedLetters.push(guess)
-}
-    if (isUnique && isBadGuess) {
-        this.remainingGuesses = this.remainingGuesses - 1
-        // this.remainingGuesses--
+        this.remainingGuesses = this.remainingGuesses - 1 
+ 
+    } else if (this.guessedLetters.includes(guess) || this.word.includes(guess)) {
+        this.guessedLetters.push(guess)
+        this.remainingGuesses = this.remainingGuesses 
     }
 }
+*/
+// Display the puzzle to the browser instead of the console.
+// Display the guesses left to the browser instead of the console.
+// Separate the Hangman definition from the rest of the app code.
 
-const game1 = new Hangman('cat', 2);
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
 
-window.addEventListener('keypress', function (e) {
-    const guess = String.fromCharCode(e.charCode)
-    console.log(game1.getPuzzle())
-    console.log(game1.remainingGuesses)
-})
+
+
+
+
+
+
 
