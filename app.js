b927ff8b14cb4f6ba35b28d798566f7a3ca03e1d
John Correa <ema8720@gmail.com>
2021-05-22 11:16:56 -0700

    added a error handling on the http request

--- app.js
+++ app.js
@@ -48,11 +48,14 @@ const request = new XMLHttpRequest()
 
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 ) {
+    if (e.target.readyState === 4 && e.target.status === 200 ) {
+        console.log(e.target.status);
         const data = JSON.parse(e.target.responseText)
         console.log(data);
+    } else if (e.target.readyState === 4) {
+        console.log("An error has taken place");
     }
-})
+}) 
 
 request.open('GET', 'http://puzzle.mead.io/puzzle')
 request.send()
