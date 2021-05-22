8965fbcad032d290158834b34190e8e5b8930680
John Correa <ema8720@gmail.com>
2021-05-22 16:22:28 -0700

    created an asynchronousexecution

--- app.js
+++ app.js
@@ -44,18 +44,38 @@ window.addEventListener('keypress', function(e) {
 // Request -- What do we want to do.
 // Response -- What was actually done. 
 
-const request = new XMLHttpRequest()
-
-
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200 ) {
-        console.log(e.target.status);
-        const data = JSON.parse(e.target.responseText)
-        console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log("An error has taken place");
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(puzzle);
     }
-}) 
+   
+})
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
-request.send()
+
+
+
+// // 1. Make a new request for al countries
+// // 2. Parse the responseText to get back they array of objects
+// // 3. Find your country object by it's country code (alpha2Code property)
+// // 4. Print the full country name (name property)
+
+// const countryCode = "US"
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) =>  
+//             country.alpha2Code === countryCode) 
+//             console.log(country.name);
+//     } else if  (e.target.readyState === 4) {
+//         console.log("Unable to fetch data.");
+//     }
+// })
+
+
+// countryRequest.open('GET', "http://restcountries.eu/rest/v2/all" )
+// countryRequest.send()
+
