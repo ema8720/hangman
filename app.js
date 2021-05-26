fb3076fc0eb0926aa8d14880ca35874b1f1ad4cd
John Correa <ema8720@gmail.com>
2021-05-25 23:07:11 -0700

    I have converted to a promise api from a callback

--- app.js
+++ app.js
@@ -44,23 +44,17 @@ window.addEventListener('keypress', function(e) {
 // Request -- What do we want to do.
 // Response -- What was actually done. 
 
-getPuzzle("1",(error, puzzle) => {
-    if (error) {
-        console.log(`Error: ${error}`);
-    } else {
-        console.log(puzzle);
-    }
-   
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle);
+}, (err) => {
+    console.log(`Error: ${err}`);
 })
 
 
-getCountry('PR', (error, country) => {
-    if (error) {
-        console.log(error);
-    } else {
-      console.log(`Country name: ${country.name}`);  
-    }
-
+getCountry('MX').then((country) => {
+    console.log(country.name);
+}, (err) => {
+    console.log(`Error: ${err} `);
 })
 
 // 1. Create a new function for getting country details
